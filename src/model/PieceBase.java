1d540b7106e7df95f3cbe11f6d8d939454a09300
xi.yang <1006652872@qq.com>
2017-06-18 15:43:52 +0800

    update

--- src/model/PieceBase.java
+++ src/model/PieceBase.java
@@ -3,5 +3,23 @@ package model;
 /**
  * Created by bai on 2017/6/17.
  */
-public class PieceBase {
+public abstract class PieceBase {
+    private PieceType pieceType;
+    private int[] location;
+
+    public PieceType getPieceType() {
+        return pieceType;
+    }
+
+    public void setPieceType(PieceType pieceType) {
+        this.pieceType = pieceType;
+    }
+
+    public int[] getLocation() {
+        return location;
+    }
+
+    public void setLocation(int[] location) {
+        this.location = location;
+    }
 }
--- src/model/PieceType.java
+++ src/model/PieceType.java
@@ -3,5 +3,36 @@ package model;
 /**
  * Created by bai on 2017/6/17.
  */
-public class PieceType {
+public enum PieceType {
+    J("将","j"),
+    C("车","c"),
+    M("马","m"),
+    P("炮","p"),
+    S("士","s"),
+    X("相","x"),
+    Z("卒","z")
+    ;
+    private String name;
+    private String tag;
+
+    PieceType(String name, String tag) {
+        this.name = name;
+        this.tag = tag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
